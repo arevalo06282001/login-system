0e766c886e28cf74f86487506b7da0f899412dec
arevalo06282001 <arevaloaaronn3@gmail.com>
2025-09-18 13:32:00 +0800

    Update UserController.php

--- login-system/app/Http/Controllers/UserController.php
+++ login-system/app/Http/Controllers/UserController.php
@@ -80,7 +80,7 @@ class UserController extends Controller
     {
         // Find the user or throw a 404 if not found
         $user = User::findOrFail($id);
-        return view('edit', compact('user'));
+        return view('users.edit', compact('user'));
     }
 
     public function update(Request $request, $id)
